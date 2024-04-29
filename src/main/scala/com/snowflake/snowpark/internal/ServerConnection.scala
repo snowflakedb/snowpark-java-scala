e8efdf1f59d1d9618ae4dcfc08b494b79fdb564d
Bing Li <bing.li@snowflake.com>
2024-04-29 12:02:29 -0700

    upgrade jdbc

--- src/main/scala/com/snowflake/snowpark/internal/ServerConnection.scala
+++ src/main/scala/com/snowflake/snowpark/internal/ServerConnection.scala
@@ -3,11 +3,37 @@ package com.snowflake.snowpark.internal
 import java.io.{Closeable, InputStream}
 import java.sql.{PreparedStatement, ResultSetMetaData, SQLException, Statement}
 import java.time.LocalDateTime
-import com.snowflake.snowpark.{MergeBuilder, MergeTypedAsyncJob, Row, SnowparkClientException, TypedAsyncJob}
-import com.snowflake.snowpark.internal.ParameterUtils.{ClosureCleanerMode, DEFAULT_MAX_FILE_DOWNLOAD_RETRY_COUNT, DEFAULT_MAX_FILE_UPLOAD_RETRY_COUNT, DEFAULT_REQUEST_TIMEOUT_IN_SECONDS, DEFAULT_SNOWPARK_USE_SCOPED_TEMP_OBJECTS, MAX_REQUEST_TIMEOUT_IN_SECONDS, MIN_REQUEST_TIMEOUT_IN_SECONDS, SnowparkMaxFileDownloadRetryCount, SnowparkMaxFileUploadRetryCount, SnowparkRequestTimeoutInSeconds, Url}
+import com.snowflake.snowpark.{
+  MergeBuilder,
+  MergeTypedAsyncJob,
+  Row,
+  SnowparkClientException,
+  TypedAsyncJob
+}
+import com.snowflake.snowpark.internal.ParameterUtils.{
+  ClosureCleanerMode,
+  DEFAULT_MAX_FILE_DOWNLOAD_RETRY_COUNT,
+  DEFAULT_MAX_FILE_UPLOAD_RETRY_COUNT,
+  DEFAULT_REQUEST_TIMEOUT_IN_SECONDS,
+  DEFAULT_SNOWPARK_USE_SCOPED_TEMP_OBJECTS,
+  MAX_REQUEST_TIMEOUT_IN_SECONDS,
+  MIN_REQUEST_TIMEOUT_IN_SECONDS,
+  SnowparkMaxFileDownloadRetryCount,
+  SnowparkMaxFileUploadRetryCount,
+  SnowparkRequestTimeoutInSeconds,
+  Url
+}
 import com.snowflake.snowpark.internal.Utils.PackageNameDelimiter
 import com.snowflake.snowpark.internal.analyzer.{Attribute, Query, SnowflakePlan}
-import net.snowflake.client.jdbc.{FieldMetadata, SnowflakeConnectString, SnowflakeConnectionV1, SnowflakeReauthenticationRequest, SnowflakeResultSet, SnowflakeResultSetMetaData, SnowflakeStatement}
+import net.snowflake.client.jdbc.{
+  FieldMetadata,
+  SnowflakeConnectString,
+  SnowflakeConnectionV1,
+  SnowflakeReauthenticationRequest,
+  SnowflakeResultSet,
+  SnowflakeResultSetMetaData,
+  SnowflakeStatement
+}
 import com.snowflake.snowpark.types._
 import net.snowflake.client.core.QueryStatus
 
@@ -39,8 +65,11 @@ private[snowpark] object ServerConnection {
 //        .getFields
 //        .asScala
 //        .toList
-      val fieldMetadata = meta.asInstanceOf[SnowflakeResultSetMetaData]
-          .getColumnFields(index).asScala.toList
+      val fieldMetadata = meta
+        .asInstanceOf[SnowflakeResultSetMetaData]
+        .getColumnFields(index)
+        .asScala
+        .toList
       val columnName = analyzer.quoteNameWithoutUpperCasing(meta.getColumnLabel(index))
       val dataType = meta.getColumnType(index)
       val fieldSize = meta.getPrecision(index)
