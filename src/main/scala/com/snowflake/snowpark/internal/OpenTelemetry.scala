ecf515f09829d615cbb0b2d7edff8576d312ee56
Bing Li <bing.li@snowflake.com>
2024-07-19 14:30:04 -0700

    temp udf

--- src/main/scala/com/snowflake/snowpark/internal/OpenTelemetry.scala
+++ src/main/scala/com/snowflake/snowpark/internal/OpenTelemetry.scala
@@ -1,11 +1,53 @@
 package com.snowflake.snowpark.internal
 
+import com.snowflake.snowpark.UserDefinedFunction
 import io.opentelemetry.api.GlobalOpenTelemetry
 import io.opentelemetry.api.trace.{Span, StatusCode}
 
 import scala.util.DynamicVariable
 
 object OpenTelemetry extends Logging {
+
+  private val udfInfo = new DynamicVariable[Option[UdfInfo]](None)
+
+  def udf(
+      className: String,
+      funcName: String,
+      execName: String,
+      execHandler: String,
+      execFilePath: String,
+      stackOffset: Int)(func: => UserDefinedFunction): UserDefinedFunction = {
+    try {
+      udfInfo.withValue[UserDefinedFunction](udfInfo.value match {
+        // empty info means this is the entry of the recursion
+        case None =>
+          val stacks = Thread.currentThread().getStackTrace
+          val index = 4 + stackOffset
+          val fileName = stacks(index).getFileName
+          val lineNumber = stacks(index).getLineNumber
+          Some(
+            UdfInfo(
+              className,
+              funcName,
+              fileName,
+              lineNumber,
+              execName,
+              execHandler,
+              execFilePath))
+        // if value is not empty, this function call should be recursion.
+        // do not issue new SpanInfo, use the info inherited from previous.
+        case other => other
+      }) {
+        val result: UserDefinedFunction = func
+        OpenTelemetry.emit(udfInfo.value.get)
+        result
+      }
+    } catch {
+      case error: Throwable =>
+        OpenTelemetry.reportError(className, funcName, error)
+        throw error
+    }
+  }
 
   // only report the top function info in case of recursion.
   private val actionInfo = new DynamicVariable[Option[ActionInfo]](None)
@@ -40,6 +82,7 @@ object OpenTelemetry extends Logging {
         throw error
     }
   }
+
   // class name format: snow.snowpark.<class name>
   // method chain: Dataframe.filter.join.select.collect
   def emit(spanInfo: ActionInfo): Unit =
@@ -48,6 +91,17 @@ object OpenTelemetry extends Logging {
         span.setAttribute("code.filepath", spanInfo.fileName)
         span.setAttribute("code.lineno", spanInfo.lineNumber)
         span.setAttribute("method.chain", spanInfo.methodChain)
+      }
+    }
+
+  def emit(udfInfo: UdfInfo): Unit =
+    emit(udfInfo.className, udfInfo.funcName) { span =>
+      {
+        span.setAttribute("code.filepath", udfInfo.fileName)
+        span.setAttribute("code.lineno", udfInfo.lineNumber)
+        span.setAttribute("snow.executable.name", udfInfo.execName)
+        span.setAttribute("snow.executable.handler", udfInfo.execHandler)
+        span.setAttribute("snow.executable.filepath", udfInfo.execFilePath)
       }
     }
 
@@ -87,3 +141,12 @@ case class ActionInfo(
     fileName: String,
     lineNumber: Int,
     methodChain: String)
+
+case class UdfInfo(
+    className: String,
+    funcName: String,
+    fileName: String,
+    lineNumber: Int,
+    execName: String,
+    execHandler: String,
+    execFilePath: String)
