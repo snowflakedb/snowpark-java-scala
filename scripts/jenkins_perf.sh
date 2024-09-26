ebea9a2b78775c9305c284fd738be785f07ff586
Bing Li <bing.li@snowflake.com>
2024-09-25 18:25:11 -0700

    perf test

--- scripts/jenkins_perf.sh
+++ scripts/jenkins_perf.sh
@@ -4,6 +4,6 @@
 gpg --quiet --batch --yes --decrypt --passphrase="$GPG_KEY" --output profile.properties scripts/profile.properties.gpg
 gpg --quiet --batch --yes --decrypt --passphrase="$GPG_KEY" --output snowhouse.properties scripts/snowhouse.properties.gpg
 
-mvn clean compile
-mvn -Dgpg.skip -DtagsToInclude=com.snowflake.snowpark.PerfTest -DargLine="-DPERF_TEST=true -Xss1G" test
+sbt clean compile
+sbt "testOnly com.snowflake.snowpark.PerfTest" -J-DargLine="-DPERF_TEST=true -Xss1G"
 
