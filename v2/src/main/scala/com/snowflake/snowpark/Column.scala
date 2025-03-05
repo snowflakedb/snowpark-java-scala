b0c12ce873b822d082d30eb836d8f7591b29652c
Bing Li <63471091+sfc-gh-bli@users.noreply.github.com>
2025-03-05 09:51:36 -0800

    SNOW-1959555 Create Conversion Function to Convert Scala Literal to ProtoBuf Expr (#191)
    * add expr handler
    
    * add literal to expr
    
    * add test

--- v2/src/main/scala/com/snowflake/snowpark/Column.scala
+++ v2/src/main/scala/com/snowflake/snowpark/Column.scala
@@ -1,3 +1,6 @@
 package com.snowflake.snowpark
 
-class Column {}
+import com.snowflake.snowpark.internal.AstFunc
+import com.snowflake.snowpark.proto.ast.Expr
+
+case class Column(override private[snowpark] val ast: Expr) extends AstFunc {}
