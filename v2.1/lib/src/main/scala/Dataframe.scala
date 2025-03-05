0be166fa0a4854dabae1649b3ff61c20e5bb891c
Arthur Zwiegincew <arthur.zwiegincew@snowflake.com>
2025-03-05 10:36:31 -0800

    ...

--- v2.1/lib/src/main/scala/Dataframe.scala
+++ v2.1/lib/src/main/scala/Dataframe.scala
@@ -5,7 +5,7 @@ import scala.language.experimental.macros
 class Dataframe(m: Int) {
   def fillna(n: Int): Int = macro DataframeMacro.fillna
 
-  def fillnaImpl(n: Int, src: DataframeMacro.SrcPosition): Int = {
+  def fillnaImpl(n: Int, src: SrcPosition): Int = {
     println(s"src $src")
     m + n
   }
--- v2.1/macro/src/main/scala/macro.scala
+++ v2.1/macro/src/main/scala/macro.scala
@@ -3,13 +3,9 @@ package com.snowflake.snowpark
 import scala.language.experimental.macros
 import scala.reflect.macros.blackbox
 
-case class SrcPosition(filename: String, line: Int)
+case class SrcPosition(filename: String, line: Int, column: Int)
 
 object DataframeMacro {
-  // TODO: provide an instance of Liftable[_] for `SrcPosition` above.
-  // ...or maybe just an implicit conversion from this tuple type to SrcPosition.
-  type SrcPosition = (String, Int, Int)
-
   def fillna(c: blackbox.Context)(n: c.Expr[Int]): c.Expr[Int] = {
     import c.universe._
 
@@ -22,7 +18,7 @@ object DataframeMacro {
     // TODO: more to explore here -- we do get the beginning/end point of the entire invocation.
 
     c.Expr[Int] {
-      q"""$self.fillnaImpl($n, ($filename, $line, $column))"""
+      q"""$self.fillnaImpl($n, com.snowflake.snowpark.SrcPosition($filename, $line, $column))"""
     }
   }
 }
