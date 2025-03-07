ae05675c20ef09795909f580498418dc654368ad
Bing Li <bing.li@snowflake.com>
2025-03-07 12:52:59 -0800

    fix test

--- v2/src/test/scala/com/snowflake/snowpark/AstUtilsSuite.scala
+++ v2/src/test/scala/com/snowflake/snowpark/AstUtilsSuite.scala
@@ -4,97 +4,132 @@ import com.google.protobuf.ByteString
 import com.snowflake.snowpark.proto.ast._
 import com.snowflake.snowpark.proto.ast.Expr.Variant
 import com.snowflake.snowpark.internal.AstUtils._
+import com.snowflake.snowpark.internal.SrcPositionInfo
 
 class AstUtilsSuite extends UnitTestBase {
 
+  implicit val dummySrcPositionInfo: SrcPositionInfo = SrcPositionInfo("test", 1, 12)
+
+  def checkAstWithSrcPosition(
+      expected: Option[SrcPosition] => Any,
+      actual: SrcPositionInfo => Expr)(implicit srcPositionInfo: SrcPositionInfo): Unit = {
+    val srcPosition = createSroPosition(srcPositionInfo)
+    val expectedExprWithSrc: Expr = expected(Some(srcPosition)) match {
+      case expr: Expr => expr
+      case variant: Variant => Expr(variant)
+    }
+    val expectedExprWithoutSrc: Expr = expected(None) match {
+      case expr: Expr => expr
+      case variant: Variant => Expr(variant)
+    }
+    // with src position
+    checkAst(expectedExprWithSrc, actual(srcPositionInfo))
+    // without src position
+    checkAst(expectedExprWithoutSrc, actual(null))
+  }
+
   test("createExpr with null value") {
-    checkAst(Expr(Variant.NullVal(NullVal())), createExpr(null, null))
+    checkAstWithSrcPosition(src => Variant.NullVal(NullVal(src = src)), createExpr(null, _))
   }
 
   test("createExpr with Expr value") {
     val expr = Expr(Variant.StringVal(StringVal(v = "test")))
-    checkAst(expr, createExpr(expr, null))
+    checkAstWithSrcPosition(_ => expr, createExpr(expr, _))
   }
 
   test("createExpr with Variant value") {
     val variant = Variant.StringVal(StringVal(v = "test"))
-    checkAst(Expr(variant), createExpr(variant, null))
+    checkAstWithSrcPosition(_ => variant, createExpr(variant, _))
   }
 
   test("createExpr with Column value") {
     val expr = Expr(Variant.StringVal(StringVal(v = "test")))
     val column = Column(expr)
-    checkAst(expr, createExpr(column, null))
+    checkAstWithSrcPosition(_ => expr, createExpr(column, _))
   }
 
   test("createExpr with String value") {
-    val expr = Expr(Variant.StringVal(StringVal(v = "test")))
-    checkAst(expr, createExpr("test", null))
+    checkAstWithSrcPosition(
+      src => Variant.StringVal(StringVal(v = "test", src = src)),
+      createExpr("test", _))
   }
 
   test("createExpr with Char value") {
-    val expr = Expr(Variant.StringVal(StringVal(v = "t")))
-    checkAst(expr, createExpr('t', null))
+    checkAstWithSrcPosition(
+      src => Variant.StringVal(StringVal(v = "t", src = src)),
+      createExpr('t', _))
   }
 
   test("createExpr with Int value") {
-    val expr = Expr(Variant.Int64Val(Int64Val(v = 1.toLong)))
-    checkAst(expr, createExpr(1, null))
+    checkAstWithSrcPosition(
+      src => Variant.Int64Val(Int64Val(v = 1.toLong, src = src)),
+      createExpr(1, _))
   }
 
   test("createExpr with Byte value") {
-    val expr = Expr(Variant.Int64Val(Int64Val(v = 1.toLong)))
-    checkAst(expr, createExpr(1.toByte, null))
+    checkAstWithSrcPosition(
+      src => Variant.Int64Val(Int64Val(v = 1.toLong, src = src)),
+      createExpr(1.toByte, _))
   }
 
   test("createExpr with Short value") {
-    val expr = Expr(Variant.Int64Val(Int64Val(v = 1.toLong)))
-    checkAst(expr, createExpr(1.toShort, null))
+    checkAstWithSrcPosition(
+      src => Variant.Int64Val(Int64Val(v = 1.toLong, src = src)),
+      createExpr(1.toShort, _))
   }
 
   test("createExpr with Long value") {
-    val expr = Expr(Variant.Int64Val(Int64Val(v = 1.toLong)))
-    checkAst(expr, createExpr(1.toLong, null))
+    checkAstWithSrcPosition(
+      src => Variant.Int64Val(Int64Val(v = 1.toLong, src = src)),
+      createExpr(1.toLong, _))
   }
 
   test("createExpr with Float value") {
-    val expr = Expr(Variant.Float64Val(Float64Val(v = 1.0)))
-    checkAst(expr, createExpr(1.0f, null))
+    checkAstWithSrcPosition(
+      src => Variant.Float64Val(Float64Val(v = 1.0, src = src)),
+      createExpr(1.0f, _))
   }
 
   test("createExpr with Double value") {
-    val expr = Expr(Variant.Float64Val(Float64Val(v = 1.0)))
-    checkAst(expr, createExpr(1.0, null))
+    checkAstWithSrcPosition(
+      src => Variant.Float64Val(Float64Val(v = 1.0, src = src)),
+      createExpr(1.0, _))
   }
 
   test("createExpr with Boolean value") {
-    val expr = Expr(Variant.BoolVal(BoolVal(v = true)))
-    checkAst(expr, createExpr(true, null))
+    checkAstWithSrcPosition(
+      src => Variant.BoolVal(BoolVal(v = true, src = src)),
+      createExpr(true, _))
   }
 
   test("createExpr with BigDecimal value") {
-    val expr = Expr(
-      Variant.BigDecimalVal(
-        BigDecimalVal(
-          scale = 2,
-          unscaledValue =
-            ByteString.copyFrom(java.math.BigDecimal.valueOf(12.34).unscaledValue().toByteArray))))
-    checkAst(expr, createExpr(BigDecimal(12.34), null))
+    checkAstWithSrcPosition(
+      src =>
+        Variant.BigDecimalVal(
+          BigDecimalVal(
+            scale = 2,
+            unscaledValue =
+              ByteString.copyFrom(java.math.BigDecimal.valueOf(12.34).unscaledValue().toByteArray),
+            src = src)),
+      createExpr(BigDecimal(12.34), _))
   }
 
   test("createExpr with JavaBigDecimal value") {
-    val expr = Expr(
-      Variant.BigDecimalVal(
-        BigDecimalVal(
-          scale = 2,
-          unscaledValue =
-            ByteString.copyFrom(java.math.BigDecimal.valueOf(12.34).unscaledValue().toByteArray))))
-    checkAst(expr, createExpr(java.math.BigDecimal.valueOf(12.34), null))
+    checkAstWithSrcPosition(
+      src =>
+        Variant.BigDecimalVal(
+          BigDecimalVal(
+            scale = 2,
+            unscaledValue =
+              ByteString.copyFrom(java.math.BigDecimal.valueOf(12.34).unscaledValue().toByteArray),
+            src = src)),
+      createExpr(java.math.BigDecimal.valueOf(12.34), _))
   }
 
   test("createExpr with Array[Byte] value") {
-    val expr = Expr(Variant.BinaryVal(BinaryVal(v = ByteString.copyFrom(Array[Byte](1, 2, 3)))))
-    checkAst(expr, createExpr(Array[Byte](1, 2, 3), null))
+    checkAstWithSrcPosition(
+      src => Variant.BinaryVal(BinaryVal(v = ByteString.copyFrom(Array[Byte](1, 2, 3)), src = src)),
+      createExpr(Array[Byte](1, 2, 3), _))
   }
 
   test("createExpr with unsupported value type") {
