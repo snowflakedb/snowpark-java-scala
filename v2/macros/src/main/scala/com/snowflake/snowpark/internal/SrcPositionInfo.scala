d89bcb7c1d1307d6c2c80ee8b7478d5ade4a5349
Bing Li <bing.li@snowflake.com>
2025-03-07 10:33:10 -0800

    rename SrcPosition

--- v2/macros/src/main/scala/com/snowflake/snowpark/internal/SrcPositionInfo.scala
+++ v2/macros/src/main/scala/com/snowflake/snowpark/internal/SrcPositionInfo.scala
@@ -3,11 +3,11 @@ package com.snowflake.snowpark.internal
 import scala.language.experimental.macros
 import scala.reflect.macros.blackbox
 
-case class SrcPosition(filename: String, line: Int, column: Int)
-object SrcPosition {
-  implicit def srcPosition: SrcPosition = macro getSrcPosition
+case class SrcPositionInfo(filename: String, line: Int, column: Int)
+object SrcPositionInfo {
+  implicit def srcPosition: SrcPositionInfo = macro getSrcPosition
 
-  def getSrcPosition(c: blackbox.Context): c.Expr[SrcPosition] = {
+  def getSrcPosition(c: blackbox.Context): c.Expr[SrcPositionInfo] = {
     import c.universe._
     val pos = c.enclosingPosition
     val filename = pos.source.file.name
@@ -16,7 +16,7 @@ object SrcPosition {
 
     // scalastyle:off
     c.Expr(
-      q"com.snowflake.snowpark.internal.SrcPosition($filename, $line, $column)")
+      q"com.snowflake.snowpark.internal.SrcPositionInfo($filename, $line, $column)")
     // scalastyle:on
   }
 }
