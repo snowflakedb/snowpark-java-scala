d3d6e2345af0852c63397df5f82f18874a7a82aa
Mau Vega <mau.vega@snowflake.com>
2025-08-25 19:02:37 -0600

    apply 'sbt +test:javafmt' to fix Scala Test Code Format Check: Failed!

--- src/test/java/com/snowflake/snowpark_test/JavaFunctionSuite.java
+++ src/test/java/com/snowflake/snowpark_test/JavaFunctionSuite.java
@@ -1239,7 +1239,8 @@ public class JavaFunctionSuite extends TestBase {
   @Test
   public void try_to_timestamp() {
     DataFrame df =
-      getSession().sql("select * from values(1561479557),(1565479557),(1161479557),('Invalid') as T(a)");
+        getSession()
+            .sql("select * from values(1561479557),(1565479557),(1161479557),('Invalid') as T(a)");
     Row[] expected = {
       Row.create(Timestamp.valueOf("2019-06-25 16:19:17.0")),
       Row.create(Timestamp.valueOf("2019-08-10 23:25:57.0")),
@@ -1248,14 +1249,15 @@ public class JavaFunctionSuite extends TestBase {
     };
     checkAnswer(df.select(Functions.try_to_timestamp(df.col("a"))), expected);
 
-    DataFrame df2 = getSession().sql("select * from values('04/05/2020 01:02:03'),('Invalid') as T(a)");
+    DataFrame df2 =
+        getSession().sql("select * from values('04/05/2020 01:02:03'),('Invalid') as T(a)");
     Row[] expected2 = {
-      Row.create(Timestamp.valueOf("2020-04-05 01:02:03.0")),
-      Row.create((Object) null)
+      Row.create(Timestamp.valueOf("2020-04-05 01:02:03.0")), Row.create((Object) null)
     };
     checkAnswer(
-            df2.select(Functions.try_to_timestamp(df2.col("a"), Functions.lit("mm/dd/yyyy hh24:mi:ss"))),
-            expected2);
+        df2.select(
+            Functions.try_to_timestamp(df2.col("a"), Functions.lit("mm/dd/yyyy hh24:mi:ss"))),
+        expected2);
   }
 
   @Test
@@ -1277,18 +1279,13 @@ public class JavaFunctionSuite extends TestBase {
   @Test
   public void try_to_date() {
     DataFrame df = getSession().sql("select * from values('2020-05-11'), ('Invalid') as T(a)");
-    Row[] expected = {
-            Row.create(new Date(120, 4, 11)),
-            Row.create((Object) null)
-    };
+    Row[] expected = {Row.create(new Date(120, 4, 11)), Row.create((Object) null)};
     checkAnswer(df.select(Functions.try_to_date(df.col("a"))), expected);
 
     DataFrame df1 = getSession().sql("select * from values('2020.07.23'), ('Invalid') as T(a)");
-    Row[] expected1 = {
-            Row.create(new Date(120, 6, 23)),
-            Row.create((Object) null)
-    };
-    checkAnswer(df1.select(Functions.try_to_date(df.col("a"), Functions.lit("YYYY.MM.DD"))), expected1);
+    Row[] expected1 = {Row.create(new Date(120, 6, 23)), Row.create((Object) null)};
+    checkAnswer(
+        df1.select(Functions.try_to_date(df.col("a"), Functions.lit("YYYY.MM.DD"))), expected1);
   }
 
   @Test
