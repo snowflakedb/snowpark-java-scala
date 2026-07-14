284279ce9de793e778aeb7f0fb0da7d35df1ce88
Jing Zhan <jing.zhan@snowflake.com>
2026-07-14 20:59:35 +0000

    lint

--- src/main/java/com/snowflake/snowpark_java/types/DayTimeIntervalType.java
+++ src/main/java/com/snowflake/snowpark_java/types/DayTimeIntervalType.java
@@ -3,8 +3,8 @@ package com.snowflake.snowpark_java.types;
 /**
  * Day-time interval data type. This maps to INTERVAL DAY TO SECOND data type in Snowflake.
  *
- * <p>At the UDF/sproc boundary, values are transmitted as Arrow MonthDayNano and surfaced to
- * Java handlers as {@link java.time.Duration}.
+ * <p>At the UDF/sproc boundary, values are transmitted as Arrow MonthDayNano and surfaced to Java
+ * handlers as {@link java.time.Duration}.
  *
  * @since 1.14.0
  */
--- src/main/scala/com/snowflake/snowpark/types/YearMonthIntervalType.scala
+++ src/main/scala/com/snowflake/snowpark/types/YearMonthIntervalType.scala
@@ -4,8 +4,7 @@ package com.snowflake.snowpark.types
  * Year-month interval data type. Mapped to INTERVAL YEAR TO MONTH Snowflake data type.
  *
  * At the UDF/sproc boundary, year-month interval values are transmitted as Arrow MonthDayNano
- * ({months=M, days=0, nanoseconds=0}) and surfaced to Java/Scala handlers as
- * [[java.time.Period]].
+ * ({months=M, days=0, nanoseconds=0}) and surfaced to Java/Scala handlers as [[java.time.Period]].
  *
  * @since 1.14.0
  */
